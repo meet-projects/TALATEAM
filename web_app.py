c3418bbac41af66f86ee8b6b81c7e5ec744863b4
Lorenzo Brown <lrnzbr@gmail.com>
2015-08-09 09:25:30 +0300

    Create web_app.py
    Initial commit

---
+++ web_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# SQLAlchemy stuff
+from database_setup import Base, Person
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('sqlite:///crudlab.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
+#YOUR WEB APP CODE GOES HERE
