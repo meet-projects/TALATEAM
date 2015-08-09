53e1541d5ca5f14a670879b02b65aef6e1d1eee5
Lorenzo Brown <lrnzbr@gmail.com>
2015-08-09 09:29:34 +0300

    Create database_setup.py
    Initial commit

---
+++ database_setup.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+#PLACE YOUR TABLE SETUP INFORMATION HERE
