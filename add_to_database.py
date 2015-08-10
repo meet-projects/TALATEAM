ccd66cb320891431bd94b87fb4e8fd8a91b19bfa
Kiosk <kiosk@cs.huji.ac.il>
2015-08-10 14:59:58 +0300

    h9i

--- add_to_database.py
+++ add_to_database.py
@@ -12,10 +12,28 @@ session = DBSession()
 
 
 
- = User(
-	name = {{}}
-	id = Column(Integer, primary_key=True)
-	picURL = {{}})
+Miley = User(
+	name = "Miley Cyrus",
+	picURL = "http://cdni.condenast.co.uk/592x888/k_n/miley-cyrus_31jan14_getty_b_592x888.jpg",
+	id = 1
+	)
+job = Question(
+	option_a = "Teacher",
+	option_b = "Singer",
+	option_c = "Doctor",
+	option_d = "Computer Engineer",
+	text = "What is my job?",
+	correct_option = "Singer", 
+	id = 1,
+	user_id = 1,
+	user = relationship(User)
+	)
+
+Emraan = User(
+	name = "Miley Cyrus",
+	picURL = "http://www.iluvcinema.in/hindi/wp-content/uploads/2014/08/Emraan-is-a-very-cool-person-Kay-Kay.jpg",
+	id = 2
+	)
 	
 	
 	
