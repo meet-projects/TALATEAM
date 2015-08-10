05c43be0d5fa85314b2444a7419404688172c071
Kiosk <kiosk@cs.huji.ac.il>
2015-08-10 11:29:54 +0300

    profile2

--- web_app.py
+++ web_app.py
@@ -14,32 +14,20 @@ session = DBSession()
 
 
 #YOUR WEB APP CODE GOES HERE
-
-@app.route("/result/<int:user_id>/<int:result>", methods=['POST'])
-def result(user_id, result):
-	user = session.query(User).filter_by(id=user_id).first()
-	return render_template("result.html", user=user, result=result)
-	
-
-@app.route("/profile/<int:user_id>/", methods=["GET", 'POST'])
-def profilePage(user_id):
-	user = session.query(User).filter_by(id=user_id).first()
-	question=session.query(Question).filter_by(user_id=user.id).first()
-	if request.method == 'GET':
-		return render_template("profile.html", user=user, question=question)
-	else:
-		pass
-		
-	
-
 @app.route('/')
+@app.route('/profiles')
 def main():
 	all_users = session.query(User).all()
 	return render_template('main_page.html', users=all_users)
-	
 
+@app.route('/profiles/<int:profile_id>')
+def viewProfile(profile_id):
+		
+	user = session.query(User).filter_by(id = profile_id).first()
+	#Get the Question for that user	
+	return render_template('profile.html')
 
-
+@app.route
 
 if __name__ == '__main__':
     app.run(debug=True)
